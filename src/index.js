22a46683a86058223389b2eb78ed693dbe8a6f0e
Teodor Levakov <t.levakov@abv.bg>
2022-06-05 13:56:26 +0200

    Add homecontroller and router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const routes = require('./routes.js');
 
 const app = express();
 
@@ -11,12 +12,7 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
-
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
-
+app.use(routes);
 
 
 
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const homeController = require('./controllers/homeController.js');
+
+const router = express.Router();
+
+router.get('/', homeController.index);
+
+module.exports = router;
